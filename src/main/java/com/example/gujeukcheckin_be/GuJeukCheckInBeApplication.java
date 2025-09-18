26fe9c1c85f3cb2eab95133bb5b50614960449f5
oijwef098234 <oijwef098234@gmail.com>
2025-09-19 01:24:55 +0900

    first commit

---
+++ src/main/java/com/example/gujeukcheckin_be/GuJeukCheckInBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gujeukcheckin_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuJeukCheckInBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuJeukCheckInBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gujeukcheckin_be/GuJeukCheckInBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gujeukcheckin_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuJeukCheckInBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

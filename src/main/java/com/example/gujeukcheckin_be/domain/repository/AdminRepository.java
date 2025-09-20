eeb4bd4d3fdbc1c906750e694dc1f77513a3e7f6
oijwef098234 <oijwef098234@gmail.com>
2025-09-20 16:27:52 +0900

    feat::Repository

--- src/main/java/com/example/gujeukcheckin_be/domain/repository/AdminRepository.java
+++ src/main/java/com/example/gujeukcheckin_be/domain/repository/AdminRepository.java
@@ -1,10 +1,10 @@
 package com.example.gujeukcheckin_be.domain.repository;
 
-import com.example.gujeukcheckin_be.domain.entity.CustomLogEntity;
+import com.example.gujeukcheckin_be.domain.entity.AdminEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomLogRepository extends JpaRepository<CustomLogEntity, Long> {
+public interface AdminRepository extends JpaRepository<AdminEntity,Long> {
 
 }
